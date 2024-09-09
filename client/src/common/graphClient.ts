de53d21c79ee975af1f0774eef8f95d3a4e2d3a6
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2024-09-09 23:38:35 +0300

    Refactor graphClient.ts to use VITE_INDEXR_ENDPOINT for the URL

--- client/src/common/graphClient.ts
+++ client/src/common/graphClient.ts
@@ -1,9 +1,7 @@
 import { Client, cacheExchange, fetchExchange } from '@urql/core';
 
-const url = import.meta.env.DEV ? 'http://localhost:8080/v1/graphql' : import.meta.env.VITE_INDEXR_ENDPOINT
-
 export const subgraphClient = new Client({
-  url: url,
+  url: import.meta.env.VITE_INDEXR_ENDPOINT,
   exchanges: [cacheExchange, fetchExchange]
 })
 
