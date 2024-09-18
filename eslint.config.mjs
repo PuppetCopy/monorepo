7c8e60522e5c1f3af5e40ffecb8ccb43bc5a798c
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2024-09-18 19:31:52 +0300

    Refactor eslint.config.mjs to include "@typescript-eslint/no-empty-object-type" rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,8 @@ export default [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn"
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
     }
   }
 ]
